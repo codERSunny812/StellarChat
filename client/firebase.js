a95516e8d6ce130cea65406da0a62330f4e6e2a6
codersunny812 <sengersunny448@gmail.com>
2024-02-08 01:27:56 +0530

    env added

--- client/firebase.js
+++ client/firebase.js
@@ -1,19 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import 'dotenv/config'
+
+// load the environmental variables from .env file
+const { REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_AUTH_DOMAIN, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCKET, REACT_APP_FIREBASE_MESSAGING_SENDER_ID, REACT_APP_FIREBASE_APP_ID, REACT_APP_FIREBASE_MEASUREMENT_ID } = process.env;
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAAGeDwS3FitEtUD8Yb3_y89qL6LMqa1Zc",
-    authDomain: "chatkro-b2b85.firebaseapp.com",
-    projectId: "chatkro-b2b85",
-    storageBucket: "chatkro-b2b85.appspot.com",
-    messagingSenderId: "210951248415",
-    appId: "1:210951248415:web:a42d404ddd3abf1705d32a",
-    measurementId: "G-DGSE9K1MFY"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
