cdcf19557983f7879c4dcbfae242328da893bc2a
codersunny812 <sengersunny448@gmail.com>
2024-03-28 21:05:25 +0530

    database connection

--- server/Database/data_connection.js
+++ server/Database/data_connection.js
@@ -1,19 +1,21 @@
 // code for the database connection
-const mongoose = require('mongoose')
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const URL = process.env.MONGO_URL;
 
-exports.connectDatabase = ()=>{
+exports.connectDatabase = () => {
+  // connect to the database
 
-    // connect to the database 
-
-    mongoose.connect(`${URL}chatkro`)
-    .then(()=>{
-console.log("the mongodb is successfully connected");
+  mongoose
+    .connect(`${URL}chatkro`)
+    .then(() => {
+      console.log("the mongodb is successfully connected");
     })
-    .catch((error)=>{
-console.log("the mongodb is not successfully connected with the database due to "+ error);
-    })
-
-}
+    .catch((error) => {
+      console.log(
+        "the mongodb is not successfully connected with the database due to " +
+          error
+      );
+    });
+};
