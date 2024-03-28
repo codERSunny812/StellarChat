2f623789b89d74cc4085dc16f404d711446eada3
codersunny812 <sengersunny448@gmail.com>
2024-03-28 20:58:36 +0530

    context api

--- client/src/Context/Auth.jsx
+++ client/src/Context/Auth.jsx
@@ -3,14 +3,14 @@ import { createContext, useState } from "react";
 export const UserStatusContext = createContext();
 
 const UserStatusContextProvider = (props) => {
-    // state variable
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // state variable
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return (
-        <UserStatusContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
-            {props.children}
-        </UserStatusContext.Provider>
-    );
+  return (
+    <UserStatusContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {props.children}
+    </UserStatusContext.Provider>
+  );
 };
 
 export default UserStatusContextProvider;
