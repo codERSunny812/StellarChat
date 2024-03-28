92e78fd3bafdab709925626446e65a09029355eb
codersunny812 <sengersunny448@gmail.com>
2024-03-28 21:03:15 +0530

    main rendering file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,13 @@
+import ReactDOM from "react-dom/client";
+import router from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import UserStatusContextProvider from "./Context/Auth.jsx";
 
-import ReactDOM from 'react-dom/client'
-import  router  from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import UserStatusContextProvider from './Context/Auth.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <UserStatusContextProvider>
       <RouterProvider router={router} />
-
     </UserStatusContextProvider>
-    
-  </>,
-)
+  </>
+);
