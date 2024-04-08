413e3596d279c49086d78c2dbeb07cf3cd3bc072
codersunny812 <sengersunny448@gmail.com>
2024-04-09 00:12:41 +0530

    utility function

--- server/utility/Cloudinary.integrate.js
+++ server/utility/Cloudinary.integrate.js
@@ -1,20 +1,23 @@
-// import {v2 as cloudinary} from 'cloudinary'
 const {v2} = require('cloudinary');
 const fs = require('fs')
 
 
+
+// clodinary confugiration
 v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_KEY_SECRET,
 })
 
+
+// function to upload the photo on the cdn server
 exports.connectCloudinary =async (localFilePath)=>{
 try {
     console.log("inside the connectCloudinary try block")
     console.log(localFilePath);
     if(!localFilePath) return null;
-    //upload the file on the cloudinary
+    //upload the file on the cloudinary server
    const response = await  v2.uploader.upload(localFilePath,{
         resource_type:"auto"
     });
