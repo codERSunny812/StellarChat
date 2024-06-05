7c51dea73323ba4e09c640740f179b8e779269d8
codersunny812 <sengersunny448@gmail.com>
2024-06-05 09:54:50 +0530

    server multer config updated

--- server/utility/MulterConfig.js
+++ server/utility/MulterConfig.js
@@ -1,8 +1,10 @@
 const multer = require("multer");
 
 
+
+console.log("multer is running")
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "./Public/Images/Uploads"),
+    destination: (req, file, cb) => cb(null, "./Public/Images"),
     filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
 });
 
