9fdc291f050d95aa22130d4439b9dd1a02a7c28a
codersunny812 <sengersunny448@gmail.com>
2024-06-10 21:41:25 +0530

    talwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,31 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      boxShadow:{
-        '3xl': 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'
+      boxShadow: {
+        "3xl": "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
       },
-      borderRadius:{
-        '4xl':'15px 0px 15px 15px',
-        '5xl': '0px 15px 15px 15px',
-        '6xl':'35px 35px 0px 0px'
-
+      borderRadius: {
+        "4xl": "15px 0px 15px 15px",
+        "5xl": "0px 15px 15px 15px",
+        "6xl": "35px 35px 0px 0px",
       },
-      gradientColorStops:theme => ({
-        'custom-purple-light': '#4B0082',
-        'custom-purple-dark': '#33006F',
+      gradientColorStops: (theme) => ({
+        "custom-purple-light": "#4B0082",
+        "custom-purple-dark": "#33006F",
       }),
-      fontFamily:{
-        'edu-nsw': ['"Edu NSW ACT Foundation"', 'cursive'],
+      fontFamily: {
+        "edu-nsw": ['"Edu NSW ACT Foundation"', "cursive"],
       },
-      
-      
     },
   },
   plugins: [],
-}
-
+};
