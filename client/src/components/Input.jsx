ca2d8e9de34f8a460305bf2009741c3057e72dde
codersunny812 <sengersunny448@gmail.com>
2024-06-10 21:46:51 +0530

    input   component

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,15 +1,21 @@
 // input component
 
-const Input = ({ label, type, value, onChange}) => {
+const Input = ({ label, type, value, onChange }) => {
   return (
     <div className="  py-5 px-3 my-1">
-      <label className=" capitalize block font-semibold text-lg text-[#24786D]" >{label}</label>
-      <input name={value} type={type} className={`border-b-2 border-black w-full outline-none  placeholder:text-gray-400 placeholder:px-2 placeholder:capitalize ${type === 'file' ? 'border-none' : ''} `}
-      onChange={onChange}
-     />
-
+      <label className=" capitalize block font-semibold text-lg text-[#24786D]">
+        {label}
+      </label>
+      <input
+        name={value}
+        type={type}
+        className={`border-b-2 border-black w-full outline-none  placeholder:text-gray-400 placeholder:px-2 placeholder:capitalize ${
+          type === "file" ? "border-none" : ""
+        } `}
+        onChange={onChange}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Input;
