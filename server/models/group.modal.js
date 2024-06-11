056aa3c53b2d1a7b43ee4e449557da3191a8db94
codersunny812 <sengersunny448@gmail.com>
2024-06-12 01:23:37 +0530

    new group model is created

---
+++ server/models/group.modal.js
@@ -0,0 +1,24 @@
+const { default: mongoose } = require("mongoose");
+
+// user modal to store the data of the user
+const groupSchema = mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    avtar:{
+    type:String,
+    required:true
+    },
+    members: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'User'
+     }],
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    }
+});
+
+exports.groupModal = mongoose.model("groupModal", groupSchema);
+
