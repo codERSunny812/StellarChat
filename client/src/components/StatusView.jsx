784a0704b9022a4a5dc4600823644db8cfbecd89
codersunny812 <sengersunny448@gmail.com>
2024-06-27 23:29:13 +0530

    user can see the upload status user

--- client/src/components/StatusView.jsx
+++ client/src/components/StatusView.jsx
@@ -1,59 +1,39 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
+import StatusModal from './StatusModel';
 
-const StatusView = ({ statuses, showAllUser, user }) => {
-  // console.log(showAllUser);
-  // console.log(statuses)
-  const [showStatus , setShowStatus] = useState(false);
+const StatusView = ({ status, showAllUser, user }) => {
+  const [showStatus, setShowStatus] = useState(false);
+
+  const allUserData = showAllUser.find(
+    (userData) => userData?.userId === status?.userId
+  );
+
+  const handleImageClick = () => {
+    setShowStatus(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowStatus(false);
+  };
+
   return (
-    <div className="flex space-x-4">
-      {statuses.map((status) => {
-        // checking the post uploaded by the loggedIn user
-        if (user?.id === status?.userId) {
-          return;
-        }
-        const allUserData = showAllUser.find(
-          (userData) => userData?.userId === status?.userId
-        );
-        return user ? (
-          <div key={status?._id}>
-            <img
-              src={allUserData?.img}
-              alt={allUserData?.fullName}
-              className="h-12 w-12 rounded-full object-cover"
-              onClick={()=> setShowStatus(true)}
-            />
-
-
-                {
-
-                    showStatus && (
-
-                    <div className="image border-2 border-red-700 absolute top-0 ">
-
-                    <img src={status?.statusImg} alt="" />
-
-                    </div>
-                    )
-
-                }
-          </div>
-        //   the image status 
-        
-
-        
-        ) : null;
-      })}
+    <div className="relative">
+      <img
+        src={allUserData?.img}
+        alt={allUserData?.fullName}
+        className="h-12 w-12 rounded-full object-cover cursor-pointer"
+        onClick={handleImageClick}
+      />
+      <StatusModal isOpen={showStatus} onClose={handleCloseModal} status={status} />
     </div>
   );
 };
 
-
-
-StatusView.propTypes={
-  statuses:PropTypes.array.isRequired,
-  showAllUser:PropTypes.array.isRequired,
-  user:PropTypes.object.isRequired
-}
+StatusView.propTypes = {
+  status: PropTypes.object.isRequired,
+  showAllUser: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired,
+};
 
 export default StatusView;
